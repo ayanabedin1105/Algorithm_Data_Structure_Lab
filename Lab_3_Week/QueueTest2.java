be52cf7512a320507e1e45ac17f95aa21a2ede05
Ayan Abedin <abedin.ayanJARVIS@yahoo.com>
2022-02-23 10:28:46 +0000

    Overflow Terminated Message

--- Lab_3_Week/QueueTest2.java
+++ Lab_3_Week/QueueTest2.java
@@ -3,6 +3,9 @@
 
 class QueueException extends Exception {
     // do as in stack example
+    public QueueException(String msg) {
+        super(msg);
+    }
 }    
 
 // In Java an interface can often be the best way to 
@@ -17,9 +20,26 @@ interface Queue {
 class QueueLL implements Queue {
     
  
-    // assume the queue is non-empty when this method is called, otherwise thro exception
+    private int size;
+
+    // assume the queue is non-empty when this method is called, otherwise throw exception
     public int deQueue() throws QueueException {
-        return deQueue();
+        if(isEmpty()) {
+            throw new QueueException("\nLL Queue is empty! ");
+        }
+        return 0;    
+    }
+
+    @Override
+    public void enQueue(int x) throws QueueException {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public boolean isEmpty() {
+        // TODO Auto-generated method stub
+        return (size == 0);
     }
 
  
@@ -91,13 +111,14 @@ class QueueCB implements Queue {
 // here we test both implementations
 class QueueTest2 {
     public static void main(String[] arg) {
-        Queue q1, q2;
+        Queue q1;
+        Queue q2;
         q1 = new QueueLL();
         q2 = new QueueCB();
         
         for(int i = 1; i<6; ++i)
         try { 
-            q1.enQueue(i);            
+            q2.enQueue(i);            
         } catch (QueueException e) {
             System.out.println("Exception thrown: " + e); 
         }
