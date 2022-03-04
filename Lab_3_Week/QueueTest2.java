9443cba215e03d4d460f5e920bcad50db5ecacf7
Ayan Abedin <abedin.ayanJARVIS@yahoo.com>
2022-03-04 00:22:53 +0000

    QueueTest2 updated

--- Lab_3_Week/QueueTest2.java
+++ Lab_3_Week/QueueTest2.java
@@ -2,7 +2,6 @@
 // and to provide 2 implementations. Also exception handling.
 
 class QueueException extends Exception {
-    // do as in stack example
     public QueueException(String msg) {
         super(msg);
     }
@@ -12,37 +11,71 @@ class QueueException extends Exception {
 // describe an Abstract Data Type (ADT) such as Queue or Stack
 interface Queue {
     public void enQueue(int x) throws QueueException;
-    public int deQueue() throws QueueException;
+    public int deQueue(int i) throws QueueException;
     public boolean isEmpty();   
+    public void display();  
 }
 
 
 class QueueLL implements Queue {
-    
- 
-    private int size;
 
-    // assume the queue is non-empty when this method is called, otherwise throw exception
-    public int deQueue() throws QueueException {
-        if(isEmpty()) {
-            throw new QueueException("\nLL Queue is empty! ");
-        }
-        return 0;    
+    private class Node {
+        int data;
+        Node next;
     }
 
-    @Override
+    Node z, head, tail;
+
     public void enQueue(int x) throws QueueException {
-        // TODO Auto-generated method stub
-        
-    }
+        Node t;
 
-    @Override
-    public boolean isEmpty() {
-        // TODO Auto-generated method stub
-        return (size == 0);
+        t = new Node();
+        t.data = x;
+        t.next = z;
+
+        if(head == z)       // case of empty list
+            head = t;
+        else                // case of list not empty
+            tail.next = t;
+            
+        tail = t;           // new node is now at the tail
+        System.out.println("LL Inserted " + x);
     }
 
  
+    // assume the queue is non-empty when this method is called, otherwise thro exception
+    public int deQueue(int x) throws QueueException {
+        if(isEmpty()){
+            throw new QueueException("\nLL Queue is empty!\n");
+        } else {
+            int deleting = head.data;
+            head = head.next;
+            System.out.println("LL Node Removed " + x);
+            return deleting;
+        }
+    }
+
+
+    public boolean isEmpty() {
+        return head == z;
+    }
+ 
+    public void display() {
+        if(isEmpty()) {
+            System.out.println("\nLL Empty Queue\n");
+        } else {
+        System.out.println("\nThe LL queue values are: ");
+
+        Node t = head;
+        while( t != null) {
+            System.out.print( t.data + "  ");
+            t = t.next;
+        }
+        System.out.println("\n");
+        }
+    }
+
+
 
 } // end of QueueLL class
 
@@ -50,60 +83,63 @@ class QueueLL implements Queue {
 
 class QueueCB implements Queue {
     private int q[], back, front;
-    private int qmax, size, capacity, count;
+    private int qmax, size;
 
  
     public QueueCB() {
-        qmax = 4;
-        size = front = back = 0;
-        count = 0;
+        qmax = 7;
+        size = back = 0;
+        front = 1;
         q = new int[qmax];
-        capacity = size;
     }
 
     public void enQueue( int x) throws QueueException  {
-        // do it
-        //check for queue overflow
-        if(isFull()) {
-            System.out.println("Overflow\n Program Terminated");
-            System.exit(-1);
+        size = q.length;
+        if(size > qmax){
+            throw new QueueException("\nCB Queue is full!\n");
+          } else {
+            if (front == -1)
+                front = 1;
+            back = (back + 1) % size;
+            q[back] = x;
+            System.out.println("CB Inserted " + x);
         }
-
-        System.out.println("Inserting " + x);
-
-        back = (back + 1) % capacity;
-        q[back] = x;
-        count++;
     }
   
-    private boolean isFull() {
-        return (size() == capacity);
-    }
-
-    private int size() {
-        return count;
-    }
-
-    public int deQueue()  throws QueueException 
+    public int deQueue(int x)  throws QueueException 
     {
-        // do it
-        if(isEmpty()) {
-            System.out.println("Underflow \nProgram Terminated");
-            System.exit(-1);
-        }
-
-        int x = q[front];
-
-        System.out.println("Removing " + x);
-
-        front = (front  + 1) % capacity;
-        count--;
-
-        return x;
-  }
+        if(isEmpty())
+            throw new QueueException("\nCB Queue is empty!\n");
+        int temp = q[front];
+        if(front == back)
+            front = back = -1;
+        else
+            front = (front+1) % q.length;
+        System.out.println("CB Element Removed " + x);
+        return temp;
+    }
 
     public boolean isEmpty() {
-        return size == 0;
+        if(size == 0){
+            return true;
+        }
+        if(front == -1){
+            return true;
+        }
+        return false;
+    }
+
+    public void display() {
+        int i;
+        if (isEmpty()) {
+            System.out.println("\nCB Empty Queue\n");
+        } else {
+            System.out.println("\nThe CB queue values are: ");
+            for (i = front; i != back; i = (i + 1) % qmax)
+                System.out.print(q[i] + "  ");
+            System.out.println(q[i]);
+            System.out.println("\n");
+        }
     }
 }
 
@@ -111,19 +147,61 @@ class QueueCB implements Queue {
 // here we test both implementations
 class QueueTest2 {
     public static void main(String[] arg) {
-        Queue q1;
-        Queue q2;
+        Queue q1, q2;
         q1 = new QueueLL();
         q2 = new QueueCB();
+
+        // EnQueue Code
+        //
         
-        for(int i = 1; i<6; ++i)
+        for(int i = 1; i<9; ++i)
         try { 
-            q2.enQueue(i);            
+            q1.enQueue(i);
+            //q1.deQueue(i);
         } catch (QueueException e) {
             System.out.println("Exception thrown: " + e); 
         }
         
-        // more test code
+         
+        q1.display();
+
+        
+        for(int i = 1; i<6; ++i)
+        try { 
+            q2.enQueue(i); 
+            //q2.deQueue(i);       
+        } catch (QueueException e) {
+            System.out.println("Exception thrown: " + e); 
+        }
+
+        q2.display(); 
+
+
+        // DeQueue code
+        //
+
+        // Linked List Queue
+        for(int i = 1; i<9; ++i)
+        try { 
+            //q1.enQueue(i);
+            q1.deQueue(i);
+        } catch (QueueException e) {
+            System.out.println("Exception thrown: " + e); 
+        }
+        
+         
+        q1.display();
+
+
+        // Circular Queue
+        for(int i = 1; i<6; ++i)
+        try { 
+            //q2.enQueue(i);     
+            q2.deQueue(i);         
+        } catch (QueueException e) {
+            System.out.println("Exception thrown: " + e); 
+        }
+
+        q2.display();
     }   
 }
-
